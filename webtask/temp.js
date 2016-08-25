67d729dc9e30e52c222a6023162c368b520ecba7
Bart Blommaerts <daggieken@hotmail.com>
2016-08-25 09:03:25 +0200

    Initial chaining from WebTask to OpenWhisk using Request. To use, replace 'apiKey' with your OpenWhisk API key.

--- webtask/temp.js
+++ webtask/temp.js
@@ -1,13 +1,30 @@
 module.exports = 
  	function(context, req, res) {
+ 		var request = require('request');
+
     	var start = new Date();
 		var temp = context.body.temp;
 
-  		res.writeHead(200, { 'Content-Type': 'application/json'});
-  		res.end(JSON.stringify({
-  			provider: "Auth 0 Webtask",
-  			start: start,
-  			temperature: temp,
-  			stop: new Date()
-  		} ));
+		var text = '{ "hops" : [{ "provider":"Auth 0 Webtask" , "start":"","temperature":"" , "stop":"" }]}';
+
+		var json = JSON.parse(text);
+		json.hops[0].start = start;
+		json.hops[0].temperature = temp;
+		json.hops[0].stop = new Date();
+
+		request({
+    		url: "https://openwhisk.ng.bluemix.net/api/v1/namespaces/Ordina_faas/actions/temperature?blocking=true",
+    		method: "POST",
+    		json: true,  
+    		body: json,
+    		headers: {
+            	'Content-Type': 'application/json',
+            	'Authorization': 'Basic apiKey'
+        	}
+		}, function (error, response, body){
+	  		res.writeHead(200, { 
+	  			'Content-Type': 'application/json',
+	  		});
+	  		res.end(JSON.stringify(response));
+  		});
     }
