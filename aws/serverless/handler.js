705acfb6725fa60ad798ab8652d6376db4698a44
Bart Blommaerts <daggieken@hotmail.com>
2016-08-26 20:45:27 +0200

    fixed post endpoint, was broken

--- aws/serverless/handler.js
+++ aws/serverless/handler.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Your first function handler
-module.exports.aws = (event, context, cb) => cb(null,
-  { message: 'Go Serverless v1.0! Your function executed successfully!', event }
+module.exports.aws_hop = (event, context, cb) => cb(null,
+  { message: 'Your function executed successfully!', event }
 );
 
 // You can add more handlers here, and reference them in serverless.yml
