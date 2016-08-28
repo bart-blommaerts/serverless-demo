c27a478e0880c5a285b3638a9912906fe2f8eb6b
Bart Blommaerts <daggieken@hotmail.com>
2016-08-28 18:51:14 +0200

    create records in Dynamodb

--- aws/serverless/handler.js
+++ aws/serverless/handler.js
@@ -1,21 +1,40 @@
 'use strict';
 
+var doc = require('dynamodb-doc');
+var uuid = require('node-uuid');
+
+var dynamo = new doc.DynamoDB();
+
 module.exports.aws_hop = function(event, context, cb) {
+	var start = new Date();
 
-  var start = new Date();
+	var hops = event.body.hops;
 
-  var hops = event.body.hops;
-
-  var lambda = {
-      "provider": "AWS Lamba - Node.js",
-      "start":  start,
-      "temperature": hops[0].temperature,
-      "stop": new Date()
+	var lambda = {
+    	"provider": "AWS Lamba - Node.js",
+    	"start":  start.toString(),
+    	"temperature": hops[0].temperature,
+    	"stop": new Date().toString()
     };
-
 	hops.push(lambda);
 
-    return cb(null,
-    	{ 'hops': hops }
-  );
+	hops.id = uuid.v1();
+	hops.updatedAt = new Date().getTime();
+
+	var params = {
+    	TableName: "hops",
+    	Item: hops
+    };
+
+   // console.log(params);
+
+	return dynamo.putItem(params, function (error, data) {
+    	if (error) {
+      		cb(error);
+    	} else {
+    		var newData = { 'hops' : params.Item };
+    		cb(error, newData);
+		} 
+	});
+
 };
